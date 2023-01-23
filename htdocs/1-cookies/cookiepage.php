95766808799255e4850fbc9ee4b7e5c3b57473bd
Jordan Pratt <jpratt@mtroyal.ca>
2023-01-23 11:57:44 +0000

    Synco.

---
+++ htdocs/1-cookies/cookiepage.php
@@ -0,0 +1,16 @@
+<?php setcookie("my-cookie", "I. Am. Cookie.") ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Cookie Cookie</title>
+</head>
+
+<body>
+    <h1>I'm Setting a Session Cookie As You Read This</h1>
+    <!-- <?php setcookie("my-cookie", "I. Am. Cookie.") ?> -->
+    <!-- If we try doing this here, baboom. Why? Check PHP.net.... -->
+</body>
+
+</html>
